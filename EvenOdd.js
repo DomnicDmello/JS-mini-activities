9b91ce5c6fe84c5ad6ce77e9627c4a2799e2b61b
Caleb Dominic Dmello <137077620+DomnicDmello@users.noreply.github.com>
2024-02-24 16:34:41 +0530

    Add files via upload

---
+++ EvenOdd.js
@@ -0,0 +1,9 @@
+let input = prompt("choose a number from 1 to 10");
+
+/*if (input % 2 === 0) {
+  alert("the chosen number is even");
+} else {
+  alert("the chosen number is odd");
+}*/
+
+input % 2 === 0 ? prompt("the number is Even") : prompt("the number is Odd");
---
+++ triplet.js
@@ -0,0 +1,9 @@
+let a = prompt("Enter the first number");
+let b = prompt("Enter the second number");
+let c = prompt("Enter the third number");
+
+if (a ** 2 + b ** 2 == c ** 2) {
+  alert("the chosen numbers are triplets");
+} else {
+  alert("the chosen numbers are not triplets");
+}
